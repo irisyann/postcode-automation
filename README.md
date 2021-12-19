a5174ead9f8fb5985fe051f236e52436fd14d338
peanutooo <48094822+peanutooo@users.noreply.github.com>
2021-12-19 11:54:28 +0800

    Edit readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # postcode-automation
 Automated postcode processing with Python.
+
+## STEP 1: clean the postcode column in excel report (trim whitespace, remove extra words)
+## STEP 2: copy and paste column to userPostCodes.csv
+## STEP 3: run without debugging
+## STEP 4: check extractedStates.txt has same number of rows as number of entries
--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import csv
 
-# STEP 1: clean the postcode column in excel report (trim whitespace, remove extra words)
-# STEP 2: copy and paste column to userPostCodes.csv
-# STEP 3: run without debugging
-# STEP 4: check extractedStates.txt has same number of rows as number of entries
-
 # open postcodes database and store into array postCodeDB
 with open('postcodes.csv', newline='') as postCodeCSV:
     postCodeDB = list(csv.reader(postCodeCSV))
